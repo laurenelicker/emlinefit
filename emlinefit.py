16d5e7a54018f7c40fe18008f384216811b65a4b
Seonwoo Kim <92557066+swkimastro@users.noreply.github.com>
2025-08-04 15:31:27 -0500

    Add fitting function with imports

--- emlinefit.py
+++ emlinefit.py
@@ -1,13 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 class emlinefit(object):
     """
-    This package will fit a gaussian to an emission line
+    This package will fit a gaussian to an emission line.
     """
-    def __init__(self, wavelength, flux):
+    def __init__(self, wavelength, flux, line_l, line_u):
         """
         emlinefit requires a wavelength array ('wavelength') and flux array ('flux')
         containing the emission line to be fit
         """
-    def gaussian(x, amp, mu, sigma):
-        exponential = -1 * (x - mu)**2 / (2 * sigma**2)
-        return amp * np.exp(exponential)
+        self.wavelength=wavelength
+        self.flux=flux
+        self.line_l=line_l
+        self.line_u=line_u
         
+    def gaussian(self, amp, mu, sigma):
+        exponential = -1 * (self.wavelength - mu)**2 / (2 * sigma**2)
+        return amp * np.exp(exponential)
+
+    def fitting(self):
+        ind = (self.wavelength>self.line_l) & (self.wavelength<self.line_u)
+        popt,pcov=curve_fit(gaussian, self.wavelength[ind], self.flux[ind],
+                    bounds=([0,self.line_l,0],[np.max(self.flux[ind]),self.line_u,self.line_u-self.line_l]))
+        return popt,pcov
