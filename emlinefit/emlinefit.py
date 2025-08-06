a8501defb5778ec575728073051fe6b3fe11bc4d
Lauren <laurenelicker2@gmail.com>
2025-08-06 11:08:33 -0400

    fix flux and wavelength path

--- emlinefit/emlinefit.py
+++ emlinefit/emlinefit.py
@@ -80,8 +80,8 @@ class emlinefit(object):
         plt.legend()
         plt.show()
         
-wavelength=np.genfromtxt('../data/wavelength_sample.txt')
-flux=np.genfromtxt('../data/flux_sample.txt')
+wavelength=np.genfromtxt('/data/wavelength_sample.txt')
+flux=np.genfromtxt('/data/flux_sample.txt')
 emline=emlinefit(wavelength, flux, 1620, 1640, fit_type='asymmetric')
 result=emline.return_result()
 print(result)
